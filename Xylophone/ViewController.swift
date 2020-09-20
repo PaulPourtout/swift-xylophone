6a929c3c967b1cbea00db8da4ae69fb3ed89208f
Paul <paul@kobusapp.com>
2020-09-20 19:38:57 +0200

    first commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    @IBAction func KeyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        if (sender.currentTitle != nil) {
+            playSound(keyName: sender.currentTitle!)
+        } else {return}
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
+    
+    func playSound(keyName: String) {
+        let url = Bundle.main.url(forResource: keyName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
+    }
 
 }
 
